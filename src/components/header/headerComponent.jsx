54f755ea039bad687ca92bf996963488dfe5c8d8
王暉霞 <wanghuixia@MacBook-Pro.local>
2021-01-21 02:06:38 +0900

    updating headerComponent

--- src/components/header/headerComponent.jsx
+++ src/components/header/headerComponent.jsx
@@ -17,7 +17,7 @@ const Header = ({ currentUser, hidden }) => (
       <Link className='option' to='/shop'>
         SHOP
       </Link>
-      <Link className='option' to='/contact'>
+      <Link className='option' to='/shop'>
         CONTACT
       </Link>
       {currentUser ? (
@@ -35,7 +35,7 @@ const Header = ({ currentUser, hidden }) => (
   </div>
 );
 
-const mapStateToProps = ({ user: { currentUser } }, { cart: { hidden } }) => ({
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
   currentUser,
   hidden,
 });
